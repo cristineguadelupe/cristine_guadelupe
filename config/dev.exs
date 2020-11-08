18647b795297346c406e439716b21c5dfb33d9fa
Cristine Guadelupe <cristineguadelupe@me.com>
2020-11-08 16:58:25 -0300

    Gigalixir configuration

--- config/dev.exs
+++ config/dev.exs
@@ -19,7 +19,7 @@ config :cristine_guadelupe, CristineGuadelupeWeb.Endpoint,
   http: [port: 4000],
   debug_errors: true,
   code_reloader: true,
-  check_origin: false,
+  check_origin: ["//cristineguadelupe.me"],
   watchers: [
     node: [
       "node_modules/webpack/bin/webpack.js",
