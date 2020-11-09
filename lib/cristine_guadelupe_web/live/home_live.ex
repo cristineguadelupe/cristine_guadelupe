795e224a5e92f9dd9ee6c39a31229b9460f8ea44
Cristine Guadelupe <cristineguadelupe@me.com>
2020-11-09 02:46:12 -0300

    Experimental home background

--- lib/cristine_guadelupe_web/live/home_live.ex
+++ lib/cristine_guadelupe_web/live/home_live.ex
@@ -10,6 +10,7 @@ defmodule CristineGuadelupeWeb.HomeLive do
     ~L"""
       <%= live_component(@socket, Console, assigns: assigns) %>
       <%= render_title(assigns) %>
+      <%= render_background(assigns) %>
       <%= render_menu(assigns) %>
     """
   end
@@ -26,6 +27,19 @@ defmodule CristineGuadelupeWeb.HomeLive do
     <%= live_component(@socket, Button, label: "soon", action: "", position: "200") %>
     <%= live_component(@socket, Button, label: "soon", action: "", position: "250") %>
     <%= live_component(@socket, Button, label: "about me", action: "about", position: "325") %>
+    """
+  end
+
+  defp render_background(assigns) do
+    ~L"""
+    <svg x="60" y="10" width="200" height="200" opacity="0.5"
+      xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+      <image href="images/bird.png" height="200" width="200"/>
+    </svg>
+    <svg x="60" y="298" width="200" height="200" opacity="0.25"
+      xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+      <image href="images/floral.png" height="200" width="200"/>
+    </svg>
     """
   end
 
